b65ddbc07ab78f70bac4bcbac0534010878a3d77
ihebessaied <i.essaied@yahoo.fr>
2021-04-17 14:14:31 +0200

    mastervide

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,8 +16,7 @@
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                                <h1>ALLLLLAL</h1>
-                                <h2>IHEHEHEHEHEB</h2>
+                               
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
