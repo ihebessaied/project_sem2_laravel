0ca35c1ae5afe4dca0fc35281e07aaf2be168074
ihebessaied <i.essaied@yahoo.fr>
2021-04-23 19:40:08 +0200

    create  event entity and model migrations and relation between user/event entity

---
+++ database/migrations/2021_04_23_154019_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+           
+            $table->rememberToken();
+            //cle etrangere
+            $table->integer('event_id')->unsigned();
+            $table->timestamps();
+            //relation user et event
+            $table->foreign('event_id')->references('id_event')->on('events')->onDelete('restrict')->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
