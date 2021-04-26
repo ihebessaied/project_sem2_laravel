ac72a327ea501a970da9b65d7c30ea5957fc6b70
alafezai <fezaiala650@gmail.com>
2021-04-26 19:35:47 +0200

    declaration of the relationship between Models

--- app/Car.php
+++ app/Car.php
@@ -2,9 +2,15 @@
 
 namespace App;
 
+use Illuminate\Console\Command;
 use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
     //
+
+    public function commande()
+    {
+        return $this->hasOne(Commande::class);
+    }
 }
--- app/Commande.php
+++ app/Commande.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Commande extends Model
 {
     //
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function Car()
+    {
+        return $this->belongsTo(Car::class);
+    }
 }
