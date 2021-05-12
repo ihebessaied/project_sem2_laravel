198c57fb8c8f5e6a8668b3d4bda59f317dc66552
AhmedAziz0047 <ahmedazizsouissi@gmail.com>
2021-05-12 01:08:11 +0100

    models relations

--- app/billet.php
+++ app/billet.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class billet extends Model
 {
     //
+    public function vole(){
+        return $this->belongsTo('App\vole');
+    }
 }
--- app/vole.php
+++ app/vole.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class vole extends Model
-{
-    //
+{       public function vole(){
+            return $this->hasMany('App\billet');
+            }
 }
