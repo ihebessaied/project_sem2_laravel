46678f488270fef203cff449150cc872e7d74ab4
bechahedfiras <firas_bechahed@hotmail.com>
2021-06-05 19:26:22 +0100

    login&register layout

--- resources/views/houses.blade.php
+++ resources/views/houses.blade.php
@@ -5,7 +5,7 @@
             <div class="container">
                 <div class="text-center">
                     <h2 class="section-heading text-uppercase">Our cars</h2>
-                    <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                    <h3 class="section-subheading text-muted">Lorem ipsum 0r sit amet consectetur.</h3>
                 </div>
                 <div class="row">
 					@foreach($houses as $h)
