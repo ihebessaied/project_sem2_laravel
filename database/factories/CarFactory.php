68948c26080c775c22310e2096b62d028edaaea4
alafezai <fezaiala650@gmail.com>
2021-06-09 14:21:12 +0100

    add 2 data get and return

--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -13,8 +13,11 @@ $factory->define(Car::class, function (Faker $faker) {
         'description' => $faker->sentence($nbWords = 6, $variableNbWords = true),
         'nbplace' => $faker->randomDigitNot(2),
         'image'=>$faker->imageUrl,
-        'mane_game' => $faker->randomDigitNot(3),
+        'mane_game' =>$faker->randomDigitNot(4),
         'prix' => $faker->numberBetween($min = 100, $max = 1000),
+        'date_get' =>$faker->dateTime($max = 'now', $timezone = null),
+        'date_return' =>$faker->dateTime($max = 'now', $timezone = null),
+        'penalite' =>$faker->randomDigitNot(1),
         'modele_id' => Modele::get('id')->random()
     ];
 });
